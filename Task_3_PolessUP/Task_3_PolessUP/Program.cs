22bc3db899c1401bbd852fc6399647947bdc57bd
AlexeyKlim4yk <114896207+AlexeyKlim4yk@users.noreply.github.com>
2023-04-09 10:29:02 +0300

    Add files via upload

---
+++ Task_3_PolessUP/Task_3_PolessUP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_3_PolessUP
+{
+    class Program
+    {
+        static int IndFindEl(int[] vs,int goal)
+        {
+            int ind = -1;
+            for (int i = 0; i < vs.Length-1; i++)
+            {
+                if (vs[i] == goal)
+                {
+                    ind = i;
+                }
+                if ( goal<vs[i+1] && goal > vs[i])
+                {
+                    ind = i+1;
+                }
+            }
+            
+            return ind;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1,2,3,4,6,7};
+            Console.WriteLine(IndFindEl(a, 5));
+        }
+    }
+}
